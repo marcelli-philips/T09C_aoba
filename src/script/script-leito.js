1822de670a7080e94fe18d18d10896615a81adbf
Carlos Daniel Sucre Cordova <carlos.cordova@philips.com>
2025-05-22 23:31:33 -0300

    Ajuste no Leitos, agora a Lista é criada a partir do LocalStore

--- src/script/script-leito.js
+++ src/script/script-leito.js
@@ -2,6 +2,12 @@ const adicionar_leito_btn = document.getElementById('btn-adicionar')
 const fechar_modal_cadastro_btn = document.getElementById('fechar-modal');
 const modal_cadastro = document.getElementById('modal-cadastro-leito');
 
+const registros = JSON.parse(localStorage.getItem("registros")) || [];
+
+const leitos = registros.filter(r => r.tipo === "leito");
+
+console.log("Registros de leitos:", leitos);
+
 adicionar_leito_btn.addEventListener('click', () =>{
     
     modal_cadastro.classList.remove('modal-overlay-hidden')
@@ -18,3 +24,34 @@ document.getElementById('toggle-menu').addEventListener('click', function () {
     const sidebar = document.querySelector('aside');
     sidebar.classList.toggle('minimized');
 });
+
+//Popular tabelas com os leitos registrados
+function popularTabelaLeitos() {
+    const registros = JSON.parse(localStorage.getItem("registros")) || [];
+    const leitos = registros.filter(r => r.tipo === "leito");
+
+    const tbody = document.querySelector(".leitos-table tbody");
+    tbody.innerHTML = ""; // limpa o conteúdo atual da tabela
+
+    leitos.forEach(leito => {
+        const tr = document.createElement("tr");
+
+        // O leito va estar livre inicialmente
+        const status = "Livre";
+        const statusClass = "livre";
+        const nomePaciente = "-";
+        const botao = `<button class="btn-acao-livre">Ocupar</button>`;
+
+        tr.innerHTML = `
+            <td>${leito.alaLeito}</td>
+            <td>${leito.numeroLeito}</td>
+            <td><span class="status ${statusClass}">${status}</span></td>
+            <td>${nomePaciente}</td>
+            <td>${botao}</td>
+        `;
+
+        tbody.appendChild(tr);
+    });
+}
+
+popularTabelaLeitos();
